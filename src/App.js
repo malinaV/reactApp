47439a53237a204a5f53af673b2ec85a9f66ce55
malinaV <mihaiki79@gmail.com>
2019-06-06 14:38:55 +0400

    1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car.js';
 
 class App extends Component {
+
+  state = {
+    cars: [
+      {
+        name: 'Ford',
+        year: 2018,
+        id: '1'
+      },
+      {
+        name: 'Audi',
+        year: 2016,
+        id: '2'
+      },
+      {
+        name: 'Mazda',
+        year: 2006,
+        id: '3'
+      }
+    ],
+    pageTitle: 'React components',
+    showCars: true
+  };
+
+  /*changeTitleHandler = (newTitle) => {
+    this.setState({
+      pageTitle: newTitle
+    });
+  };*/
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat();
+
+    cars.splice(index, 1);
+
+    this.setState({cars});
+  }
+
+  onChangeName = (name, index) => {
+    const car = this.state.cars[index];
+    car.name = name;
+    const cars = [...this.state.cars];
+    cars[index] = car;
+    this.setState({cars});
+  };
+
+  renderCars = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    });
+  };
+
   render() {
+    const divStyle = {
+      textAlign: 'center'
+    };
+
+    let cars = null;
+
+    if(this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+            <Car
+                name={car.name}
+                year={car.year}
+                key={car.id}
+                /*onChangeTitle={this.changeTitleHandler.bind(this, car.name)}*/
+                onDelete={this.deleteHandler.bind(this, index)}
+                onChangeName={event => this.onChangeName(event.target.value, index)}
+            />
+        )
+      })
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={divStyle}>
+        <h1 style={{minHeight: 38}}>{this.state.pageTitle}</h1>
+
+        <button onClick={this.renderCars}>Toggle cars</button>
+
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '20px'
+        }}>
+          { cars }
+        </div>
       </div>
     );
   }
